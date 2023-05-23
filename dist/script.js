09a7baa084f71072e3734339bb70b547ec304029
joshualcaria <alcariajoshua13@gmail.com>
2023-05-23 15:27:06 +0800

    master

--- dist/script.js
+++ dist/script.js
@@ -1,17 +1,17 @@
-const toggleMenu = document.querySelector(".toggle__menu");
-const headNav = document.querySelector(".header__actions");
-  toggleMenu.addEventListener("click", () => {
-  toggleMenu.classList.toggle("open");
-  headNav.classList.toggle("open");
-});
+// const toggleMenu = document.querySelector(".toggle__menu");
+// const headNav = document.querySelector(".header__actions");
+//   toggleMenu.addEventListener("click", () => {
+//   toggleMenu.classList.toggle("open");
+//   headNav.classList.toggle("open");
+// });
 
-const links = document.querySelectorAll(".footer__links__item h4");
-links.forEach((baba) => {
-    baba.addEventListener("click", () => {
-    baba.nextElementSibling.classList.toggle("open");
-    baba.querySelector("i").classList.toggle("open");
-    });
-});
+// const links = document.querySelectorAll(".footer__links__item h4");
+// links.forEach((baba) => {
+//     baba.addEventListener("click", () => {
+//     baba.nextElementSibling.classList.toggle("open");
+//     baba.querySelector("i").classList.toggle("open");
+//     });
+// });
 
 const faqs = document.querySelectorAll(".faq__question h4");
 faqs.forEach((faq) => {
@@ -21,28 +21,28 @@ faqs.forEach((faq) => {
     });
 });
 
-const starNav = document.querySelectorAll(".star__nav__link");
-const starContent = document.querySelectorAll(".coffee__tab__content");
-starNav.forEach((star) => {
-  star.addEventListener("click", () => {
-    removeActiveStar();
+const faqNav = document.querySelectorAll(".faq__nav__link");
+const faqContent = document.querySelectorAll(".faq__tab__content");
+faqNav.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    removeActiveFaq();
     
-    star.classList.add("active");
+    faq.classList.add("active");
    
-    const activeContent = document.querySelector(`#${star.id}-content`);
-    removeActiveContent();
-    activeContent.classList.add("active");
+    const activeFaqContent = document.querySelector(`#${faq.id}-content`);
+    removeActiveFaqContent();
+    activeFaqContent.classList.add("active");
   });
 });
 
-function removeActiveStar() {
-  starNav.forEach((star) => {
-    star.classList.remove("active");
+function removeActiveFaq() {
+  faqNav.forEach((faq) => {
+    faq.classList.remove("active");
   });
 }
 
-function removeActiveContent() {
-  starContent.forEach((star) => {
-    star.classList.remove("active");
+function removeActiveFaqContent() {
+  faqContent.forEach((faq) => {
+    faq.classList.remove("active");
   });
 }
